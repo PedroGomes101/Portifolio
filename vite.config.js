d246cfddff9cc2fbc7a0f2a2365c49ccac9d58d7
PedroGomes101 <pedrogomesdecarvalholopes11@gmail.com>
2025-11-04 20:21:35 -0300

    Fix GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: process.env.NODE_ENV === 'production' ? '/Portifolio/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
